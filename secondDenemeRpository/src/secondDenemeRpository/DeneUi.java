d131bb8fec12ffb7b818f81378a6d422bccc27e2
Onur Kırklaroğlu <okirklaroglu@uni-yaz.com>
2019-12-24 00:15:18 +0300

    Sayı eklendi...

--- secondDenemeRpository/src/secondDenemeRpository/DeneUi.java
+++ secondDenemeRpository/src/secondDenemeRpository/DeneUi.java
@@ -5,6 +5,13 @@ public class DeneUi {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Merhaba!!!");
+		
+		int i;
+		
+		for(i=0; i<10; i++)  {
+			i++;
+		}
+		System.out.println("Toplam Sayı : " + i);
 	}
 
 }
